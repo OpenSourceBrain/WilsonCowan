27739abd011d87ea2f50024ed2a99446cf3a4dca
Jessica Dafflon <jejedafflon@gmail.com>
2018-06-07 15:32:41 +0100

    Python: Add driven version of the Wilson and Cowan model

--- Python/WilsonCowan.py
+++ Python/WilsonCowan.py
@@ -16,7 +16,9 @@ def calculate_firing_rate(ie0, ie1, w, t, dt, uu, vv, wee, wie, ze, zi):
     vv_p = vv + dI
     return uu_p, vv_p
 
-
+# ------------------------------------------------------------------------------------------------------------------
+#                                                   No Drive
+# ------------------------------------------------------------------------------------------------------------------
 # settings
 wee = 10
 wie = 8
@@ -31,7 +33,51 @@ ii0 = 0
 ii1 = 0
 w = 0.25
 
-step = .025
+step = .005
+tstop = 100
+dt = np.arange(0, tstop, step)
+uu_p = np.zeros((len(dt), 1))
+vv_p = np.zeros((len(dt), 1))
+
+# initial conditions
+uu0 = 0.1
+vv0 = 0.05
+
+for dt_idx in range(len(dt)):
+    t = dt_idx * step
+    if dt_idx == 0:
+        uu_p[dt_idx], vv_p[dt_idx] = calculate_firing_rate(ie0, ie1, w, t, step, uu0, vv0, wee, wie, ze, zi)
+    else:
+        uu_p[dt_idx], vv_p[dt_idx] = calculate_firing_rate(ie0, ie1, w, t, step, uu_p[dt_idx - 1], vv_p[dt_idx - 1], wee, wie, ze, zi)
+
+plt.figure()
+plt.plot(dt, uu_p, label='uu', color='red')
+plt.plot(dt, vv_p, label='vv', color='blue')
+plt.xlabel('Time')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.plot(uu_p, vv_p)
+plt.xlabel('I')
+plt.ylabel('E')
+plt.show()
+# ------------------------------------------------------------------------------------------------------------------
+#                                                   Driven
+# ------------------------------------------------------------------------------------------------------------------
+# settings
+wei = 12
+wii = 3
+ze = 0.2
+zi = 4
+tau = 1
+ie0 = 0
+ie1 = 0.5
+ii0 = 0
+ii1 = 0.5
+w = 0.25
+
+step = .005
 tstop = 100
 dt = np.arange(0, tstop, step)
 uu_p = np.zeros((len(dt), 1))
