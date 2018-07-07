46bf65d7311a8301d52f2ace1ebecf103c142a5b
Jessica Dafflon <jejedafflon@gmail.com>
2018-07-07 20:09:40 +0100

    Python: Beautify plot label

--- Python/wilsonCowan1972.py
+++ Python/wilsonCowan1972.py
@@ -4,10 +4,12 @@ import math
 import numpy as np
 import matplotlib.pylab as plt
 
-def sigmoid(x, theta, a):
+
+def transduction_function(x, theta, a):
     return 1 / (1 + math.exp(-a * (x - theta)))
 
-def inverse_function(x, theta, a):
+
+def inverse_transduction_function(x, theta, a):
     print x, math.log((1-x)/x)
     return - 1/a * (math.log((1-x)/x)) + theta
 
@@ -34,26 +36,26 @@ E = np.zeros((len(population), 1))
 
 for e_idx, e in enumerate(population):
     x = e / (ke - (re * e))
-    I[e_idx] = ((wee * e) - inverse_function(x, theta_e, ae) + p) / wei
+    I[e_idx] = ((wee * e) - inverse_transduction_function(x, theta_e, ae) + p) / wei
 
 for i_idx, i in enumerate(population):
     x = i / (ki - (ri * i))
-    E[i_idx] = ((wii * i) + inverse_function(x, theta_i, ai) - q) / wie
+    E[i_idx] = ((wii * i) + inverse_transduction_function(x, theta_i, ai) - q) / wie
 
 plt.figure()
-plt.plot(E, population, label='dI/dt')
+plt.plot(E, population, label=r'$\frac{dI}{dt}=0$')
 plt.xlabel('E')
 plt.legend()
 plt.show()
 plt.figure()
-plt.plot(I, population, label='dE/dt')
+plt.plot(I, population, label=r'$\frac{dE}{dt}=0$')
 plt.xlabel('I')
 plt.legend()
 plt.show()
 
 plt.figure()
-plt.plot(population, E, label='dI/dt')
-plt.plot(I, population, label='dE/dt')
+plt.plot(population, E, label=r'$\frac{dI}{dt}=0$')
+plt.plot(I, population, label=r'$\frac{dE}{dt}=0$')
 plt.legend()
 plt.ylabel('E')
 plt.xlabel('I')
