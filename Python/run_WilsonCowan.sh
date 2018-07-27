da59332a9d49669d269f30f6b838662283bdebe8
Jessica Dafflon <jejedafflon@gmail.com>
2018-07-27 11:50:39 +0100

    Python: Minor corrections on the bash script that calls the Wilson and Cowan simulation

--- Python/run_WilsonCowan.sh
+++ Python/run_WilsonCowan.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
+set -e
+
 # No Drive
+echo "Runing Wilson and Cowan simulation for the no drive case..."
 python WilsonCowan.py -wee 10. -wei 12. -wie 8. -wii 3. -ze 0.2 -zi 4. -ie1 0 -ii1 0 -w 0.25
 
 # Driven
+echo "Runing Wilson and Cowan simulation for the driven case..."
 python WilsonCowan.py -wee 10. -wei 12. -wie 8. -wii 3. -ze 0.2 -zi 4. -ie1 0.5 -ii1 0.5 -w 0.25
