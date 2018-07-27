31047eda8d014e4601f42307b43df5d7d60fa1a0
Jessica Dafflon <jejedafflon@gmail.com>
2018-07-27 11:07:42 +0100

    Python: Remove redundancy
    * Remove redundancy from Python script
    * Add bash script that calls the WilsonCowan simulation with the correct
    * parameters for the driven and not driven case

--- Python/WilsonCowan.py
+++ Python/WilsonCowan.py
@@ -2,13 +2,13 @@ import math
 import sys
 import numpy as np
 import matplotlib.pylab as plt
-
+import argparse
 
 def sigmoid_function(x):
     return 1 / (1 + math.exp(-x))
 
 
-def calculate_firing_rate(ie0, ie1, ii0, ii1, w, t, dt, uu, vv, wee, wie, ze, zi):
+def calculate_firing_rate(ie0, ie1, ii0, ii1, w, t, dt, uu, vv, wee, wei, wie, wii, ze, zi):
     i_e = ie0 + ie1 * math.sin(w*t)
     i_i = ii0 + ii1 * math.sin(w*t)
     dE = dt * (-uu + sigmoid_function((wee * uu) - (wie * vv) - ze + i_e))/tau
@@ -17,22 +17,23 @@ def calculate_firing_rate(ie0, ie1, ii0, ii1, w, t, dt, uu, vv, wee, wie, ze, zi
     vv_p = vv + dI
     return uu_p, vv_p
 
-# ------------------------------------------------------------------------------------------------------------------
-#                                                   No Drive
-# ------------------------------------------------------------------------------------------------------------------
+
+parser = argparse.ArgumentParser(description='Parameters for the Wilson and Cowan Simulation')
+parser.add_argument('-wee', type=float, dest='wee', help='Weight between Excitatory - Excitatory layers')
+parser.add_argument('-wei', type=float, dest='wei', help='Weight between Excitatory - Inhibitory layers')
+parser.add_argument('-wie', type=float, dest='wie', help='Weight between Excitatory - Inhibitory layers')
+parser.add_argument('-wii', type=float, dest='wii', help='Weight between Inhibitory - Inhibitory layers')
+parser.add_argument('-ze', type=float, dest='ze', help='')
+parser.add_argument('-zi', type=float, dest='zi', help='')
+parser.add_argument('-ie1', type=float, dest='ie1', help='Current')
+parser.add_argument('-ii1', type=float, dest='ii1', help='Current')
+parser.add_argument('-w', type=float, dest='w', help='Phase')
+args = parser.parse_args()
+
 # settings
-wee = 10.
-wie = 8.
-wei = 12.
-wii = 3.
-ze = 0.2
-zi = 4.
 tau = 1.
 ie0 = 0
-ie1 = 0
 ii0 = 0
-ii1 = 0
-w = 0.25
 
 step = .005
 tstop = 100
@@ -44,80 +45,37 @@ vv_p = np.zeros((len(dt), 1))
 uu0 = 0.1
 vv0 = 0.05
 
-data_file = open('NoDrive.dat','w')
+if args.ie1 or args.ii1 != 0:
+    savefile = 'NoDrive.dat'
+else:
+    savefile = 'Drive.dat'
+
+data_file = open(savefile,'w')
 
 for dt_idx in range(len(dt)):
     t = dt_idx * step
     if dt_idx == 0:
-        uu_p[dt_idx], vv_p[dt_idx] = calculate_firing_rate(ie0, ie1, ii0, ii1, w, t, step, uu0,
-                                                           vv0, wee, wie, ze, zi)
+        uu_p[dt_idx], vv_p[dt_idx] = calculate_firing_rate(ie0, args.ie1, ii0, args.ii1, args.w, t, step, uu0,
+                                                           vv0, args.wee, args.wei, args.wie, args.wii, args.ze, args.zi)
     else:
-        uu_p[dt_idx], vv_p[dt_idx] = calculate_firing_rate(ie0, ie1, ii0, ii1, w, t, step, uu_p[dt_idx - 1],
-                                                           vv_p[dt_idx - 1], wee, wie, ze, zi)
-    data_file.write('%s\t%s\t%s\n'%(t/1000.,uu_p[dt_idx][0], vv_p[dt_idx][0]))
-    
+        uu_p[dt_idx], vv_p[dt_idx] = calculate_firing_rate(ie0, args.ie1, ii0, args.ii1, args.w, t, step, uu_p[dt_idx - 1],
+                                                           vv_p[dt_idx - 1], args.wee, args.wei, args.wie, args.wii,
+                                                           args.ze, args.zi)
+    data_file.write('%s\t%s\t%s\n'%(t/1000., uu_p[dt_idx][0], vv_p[dt_idx][0]))
 data_file.close()
 
-if not '-nogui' in sys.argv:
-    plt.figure()
-    plt.plot(dt, uu_p, label='uu', color='red')
-    plt.plot(dt, vv_p, label='vv', color='blue')
-    plt.xlabel('Time')
-    plt.legend()
 
-    plt.figure()
-    plt.plot(uu_p, vv_p)
-    plt.xlabel('I')
-    plt.ylabel('E')
-# ------------------------------------------------------------------------------------------------------------------
-#                                                   Driven
-# ------------------------------------------------------------------------------------------------------------------
-# settings
-wei = 12.
-wii = 3.
-ze = 0.2
-zi = 4.
-tau = 1.
-ie0 = 0
-ie1 = 0.5
-ii0 = 0
-ii1 = 0.5
-w = 0.25
+plt.figure()
+plt.plot(dt, uu_p, label='uu', color='red')
+plt.plot(dt, vv_p, label='vv', color='blue')
+plt.xlabel('Time')
+plt.legend()
 
-step = .005
-tstop = 100
-dt = np.arange(0, tstop, step)
-uu_p = np.zeros((len(dt), 1))
-vv_p = np.zeros((len(dt), 1))
-
-# initial conditions
-uu0 = 0.1
-vv0 = 0.05
-
-data_file = open('Driven.dat','w')
-
-for dt_idx in range(len(dt)):
-    t = dt_idx * step
-    if dt_idx == 0:
-        uu_p[dt_idx], vv_p[dt_idx] = calculate_firing_rate(ie0, ie1, ii0, ii1,  w, t, step, uu0,
-                                                           vv0, wee, wie, ze, zi)
-    else:
-        uu_p[dt_idx], vv_p[dt_idx] = calculate_firing_rate(ie0, ie1, ii0, ii1,  w, t, step, uu_p[dt_idx - 1],
-                                                           vv_p[dt_idx - 1], wee, wie, ze, zi)
-    data_file.write('%s\t%s\t%s\n'%(t/1000.,uu_p[dt_idx][0], vv_p[dt_idx][0]))
-
-data_file.close()
+plt.figure()
+plt.plot(uu_p, vv_p)
+plt.legend()
+plt.xlabel('I')
+plt.ylabel('E')
 
 if not '-nogui' in sys.argv:
-    plt.figure()
-    plt.plot(dt, uu_p, label='uu', color='red')
-    plt.plot(dt, vv_p, label='vv', color='blue')
-    plt.xlabel('Time')
-    plt.legend()
-
-    plt.figure()
-    plt.plot(uu_p, vv_p)
-    plt.xlabel('I')
-    plt.ylabel('E')
-
     plt.show()
---
+++ Python/run_WilsonCowan.sh
@@ -0,0 +1,5 @@
+# No Drive
+python WilsonCowan.py -wee 10. -wei 12. -wie 8. -wii 3. -ze 0.2 -zi 4. -ie1 0 -ii1 0 -w 0.25
+
+# Driven
+python WilsonCowan.py -wee 10. -wei 12. -wie 8. -wii 3. -ze 0.2 -zi 4. -ie1 0.5 -ii1 0.5 -w 0.25
