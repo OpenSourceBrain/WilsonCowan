d02c254dbf68458ca758473fba1d17b2fafd7399
Padraig Gleeson <p.gleeson@gmail.com>
2018-09-12 15:27:10 +0100

    Updating readme & improved nmllite

--- NEURON/README.md
+++ NEURON/README.md
@@ -2,7 +2,7 @@ Note from [original implementation on ModelDB](https://senselab.med.yale.edu/Mod
 by Bill Lytton:
 
 From Excitatory and Inhibitory Interactions in Localized Populations of Model Neurons
-Hugh R. Wilson and Jack D. Cowan; Biophys J. 1972 January; 12:1-24.
+Hugh R. Wilson and Jack D. Cowan; Biophys J. 1972 January; 12:1-24. [doi:10.1016/S0006-3495(72)86068-5](https://dx.doi.org/10.1016%2FS0006-3495(72)86068-5)
 
 To run:
 
--- NeuroML2/GenerateNetworkOverview.py
+++ NeuroML2/GenerateNetworkOverview.py
@@ -1,6 +1,6 @@
 from neuromllite.NetworkGenerator import check_to_generate_or_run
 from neuromllite import Simulation
-from neuromllite import Network, Population, Projection
+from neuromllite import Network, Population, Projection, Cell, Synapse, RectangularRegion, RandomLayout
 
 import sys
 
@@ -10,22 +10,54 @@ import sys
 net = Network(id='WC')
 net.notes = 'A simple WC network'
 
-print(net)
+r1 = RectangularRegion(id='WilsonCowan', x=0,y=0,z=0,width=1000,height=100,depth=1000)
+net.regions.append(r1)
 
-p0 = Population(id='Excitatory', size=1, component='iaf', properties={'color': '.8 0 0'})
-p1 = Population(id='Inhibitory', size=1, component='iaf', properties={'color': '0 0 .8'})
+exc_cell = Cell(id='Exc', lems_source_file='WC_Parameters.xml')
+inh_cell = Cell(id='Inh', lems_source_file='RateBased.xml') #  hack to include this file too.  
 
-print(p1.to_json())
-net.populations.append(p0)
-net.populations.append(p1)
+exc_pop = Population(id='Excitatory', 
+                     size=1, 
+                     component=exc_cell.id, 
+                     properties={'color': '.8 0 0'},
+                     random_layout = RandomLayout(region=r1.id))
+
+inh_pop = Population(id='Inhibitory', 
+                     size=1, 
+                     component=inh_cell.id, 
+                     properties={'color': '0 0 .8'},
+                     random_layout = RandomLayout(region=r1.id))
+
+net.populations.append(exc_pop)
+net.populations.append(inh_pop)
+
+exc_syn = Synapse(id='rsExc', lems_source_file='WC_Parameters.xml')
+inh_syn = Synapse(id='rsInh', lems_source_file='RateBased.xml')
+net.synapses.append(exc_syn)
+net.synapses.append(inh_syn)
 
 # Add projections
-net.projections.append(Projection(id='proj0',
-                                 presynaptic=p0.id,
-                                 postsynaptic=p1.id))
-net.projections.append(Projection(id='proj1',
-                                 presynaptic=p1.id,
-                                 postsynaptic=p0.id))
+net.projections.append(Projection(id='projEE',
+                                 presynaptic=exc_pop.id,
+                                 postsynaptic=exc_pop.id,
+                                 synapse=exc_syn.id))
+                                 
+net.projections.append(Projection(id='projEI',
+                                 presynaptic=exc_pop.id,
+                                 postsynaptic=inh_pop.id,
+                                 synapse=exc_syn.id))
+                                 
+net.projections.append(Projection(id='projIE',
+                                 presynaptic=inh_pop.id,
+                                 postsynaptic=exc_pop.id,
+                                 synapse=inh_syn.id,
+                                 weight=-1))
+                                 
+net.projections.append(Projection(id='projII',
+                                 presynaptic=inh_pop.id,
+                                 postsynaptic=inh_pop.id,
+                                 synapse=inh_syn.id,
+                                 weight=-1))
 
 # Save to JSON format
 net.id = 'WC'
@@ -33,4 +65,8 @@ print (net.to_json())
 new_file = net.to_json_file('WC.json')
 
 check_to_generate_or_run(sys.argv,
-                         Simulation(id='SimExample1',network=new_file))
+                         Simulation(id='SimExample1',
+                                    duration='100',
+                                    dt='0.025',
+                                    network=new_file,
+                                    recordRates={'all':'*'}))
