3d3a198efb565c725cb42c6bdbd8a0d49fb560cf
Padraig Gleeson <p.gleeson@gmail.com>
2019-07-24 12:16:00 +0100

    Add input

--- NeuroML2/GenerateNetworkOverview.py
+++ NeuroML2/GenerateNetworkOverview.py
@@ -1,7 +1,7 @@
 from neuromllite.NetworkGenerator import check_to_generate_or_run
 from neuromllite import Simulation
 
-from neuromllite import Network, Population, Projection, Cell, Synapse
+from neuromllite import Network, Population, Projection, Cell, Synapse, InputSource, Input
 from neuromllite import RandomConnectivity, RectangularRegion, RelativeLayout
 
 import sys
@@ -69,6 +69,33 @@ W = [['wee', 'wie'],
 # Add internal connections
 pops = [exc_pop, inh_pop]
 internal_connections(pops)
+
+# Add offset inputs
+
+net.parameters['exc_input'] = '0nA'
+net.parameters['inh_input'] = '0nA'
+net.parameters['input_delay'] = '20ms'
+net.parameters['input_duration'] = '60ms'
+
+#net.parameters['scaling'] = '1nA'
+
+input_source_e = InputSource(id='Exc_in',
+                             neuroml2_input='PulseGenerator',
+                             parameters={'amplitude':'exc_input', 'delay':'input_delay', 'duration':'input_duration'})
+net.input_sources.append(input_source_e)
+net.inputs.append(Input(id='Exc_stim',
+                        input_source=input_source_e.id,
+                        population=exc_pop.id,
+                        percentage=100))
+                        
+input_source_i = InputSource(id='Inh_in',
+                             neuroml2_input='PulseGenerator',
+                             parameters={'amplitude':'inh_input', 'delay':'input_delay', 'duration':'input_duration'})
+net.input_sources.append(input_source_i)
+net.inputs.append(Input(id='Inh_stim',
+                        input_source=input_source_i.id,
+                        population=inh_pop.id,
+                        percentage=100))
 
 # Save to JSON format
 net.id = 'WC'
